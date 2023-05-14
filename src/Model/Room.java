054ea0ea94c9763460ae1f1ad9c663b625d500c8
Ola-la5 <vsjob2019@gmail.com>
2023-05-13 23:52:17 -0700

    room updated

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -2,28 +2,53 @@ package Model;
 
 import Model.Questions.Question;
 
+import java.sql.SQLException;
+
 public class Room{
     private boolean myLock;
     private boolean myVisited;
     private Question myQuestion;
-    Room() {
+    Room() throws SQLException {
         myLock = false;
         myVisited = false;
-        myQuestion = Question.getInstance();
+
+      //  myQuestion = Question.getInstance();
     }
 
     public boolean getMyLock() {
+
         return myLock;
     }
     public boolean getMyVisited() {
+
         return myVisited;
     }
     public void setMyVisited(final boolean theVisit) {
+
         myVisited = theVisit;
     }
     public void setMyLock(final boolean theLock) {
+
         myLock = theLock;
     }
 
 
+    //the string representation of room
+        public String roomToString() {
+        final StringBuilder strb = new StringBuilder();
+        strb.append("Room is ");
+        if (myLock) {
+            strb.append("Locked");
+        } else {
+            strb.append("Unlocked");
+        }
+        strb.append(" and ");
+        if (myVisited) {
+            strb.append("Visited");
+        } else {
+            strb.append("Not Visited");
+        }
+        strb.append("\n ");
+        return strb.toString();
+    }
 }
